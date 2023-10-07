d119ed1fafc15da3b76112db10c41382cfee9f7c
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-10-07 09:48:52 -0300

    feat: config file and first implementation of heatmap

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import csv
+import json
 import math
 
 import numpy as np
+import plotly.graph_objects as go
 from scipy import stats
 
 from models.kohonen import Kohonen
 import pprint
+
 
 def load_data(path: str):
     data = []
@@ -35,12 +38,48 @@ def load_data(path: str):
     # Convert the list of lists to a NumPy array
     return np.array(data)
 
+
+def generate_heatmap(k, matrix):
+    # Create a list to store the number of countries in each position
+    num_countries_matrix = [[len(matrix[i][j]) for j in range(k)] for i in range(k)]
+
+    # Create a list to store the labels for each cell
+    labels = [[f'{num_countries_matrix[i][j]}: {", ".join(matrix[i][j])}' for j in range(k)] for i in range(k)]
+
+    # Reverse the color scale
+    colorscale = 'YlGnBu'
+
+    # Create a heatmap using Plotly
+    fig = go.Figure(data=go.Heatmap(
+        z=num_countries_matrix,
+        x=[f'Column {i + 1}' for i in range(k)],
+        y=[f'Row {i + 1}' for i in range(k)],
+        text=labels,
+        colorscale=colorscale,
+        reversescale=True,
+        colorbar=dict(title="Number of Countries")
+    ))
+
+    # Customize the layout
+    fig.update_layout(
+        title='Number of Countries in Each Position',
+        xaxis=dict(side='top'),
+    )
+
+    # Show the heatmap
+    fig.show()
+
+
 if __name__ == "__main__":
     data = load_data("./training_data/europe.csv")
 
-    model = Kohonen(len(data[0]) - 1, 4, math.sqrt(2), 0.1, data, False)
+    with open("config.json") as file:
+        config = json.load(file)
 
-    model.train(50000)
+    # se le resta 1 al tamaño del input pues el nombre del pais no se usa
+    model = Kohonen(len(data[0]) - 1, config["k"], config["radius"], config["initial_learning_rate"], data, config["initialize_random"])
+
+    model.train(config["train_limit"])
 
     resp = model.test()
 
@@ -49,6 +88,10 @@ if __name__ == "__main__":
         for col in row:
             print(f"{len(col)} ", end='')
         print()
+
+    if config["generate_heatmap"]:
+        generate_heatmap(config["k"], resp)
+
 
 
 
