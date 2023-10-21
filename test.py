6f833513c61c59398c33b0b75ff86ffa2b541770
mrojaspell <mrojaspelliccia@itba.edu.ar>
2023-10-21 19:03:26 -0300

    feat: oja graph

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy import stats
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+import plotly.express as px
+import pandas as pd
 
 from main_ej1 import load_data
 
@@ -15,6 +17,13 @@ def standardize_matrix(matrix):
 
     return np.array(np.transpose(resp))
 
+def oja_PC1_graph(ppcomponents, countries):
+    values = [item for sublist in ppcomponents for item in sublist]
+
+    df = pd.DataFrame({'Countries': countries, 'PC1 Value': values})
+    fig = px.bar(df, x='Countries', y='PC1 Value', title='PC1 Value for each country')
+    fig.show()
+
 if __name__ == "__main__":
     data = load_data("./training_data/europe.csv")
 
@@ -27,3 +36,5 @@ if __name__ == "__main__":
 
     # print(pca.components_)
     print(principalComponents)
+    #extract the names of countries which are on the first element of the list coming from load_data function as a second argument for function oja_PC1_graph
+    oja_PC1_graph(principalComponents,  [row[0] for row in data])
