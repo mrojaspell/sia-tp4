693b9178171c36f33bae324a379556e688fdd8e8
MSGronda <mgronda@itba.edu.ar>
2023-10-22 14:06:58 -0300

    fix: kohonen training loop altered
    Co-Authored-By: MarcosCasiraghi <86983868+MarcosCasiraghi@users.noreply.github.com>

--- models/kohonen.py
+++ models/kohonen.py
@@ -1,3 +1,4 @@
+import copy
 import random
 import numpy as np
 import math
@@ -26,6 +27,7 @@ def standardize_input(data):
 
     return np.array(resp)
 
+
 def calculate_euclidean_distance(vector1, vector2):
     return np.linalg.norm(vector2 - vector1)
 
@@ -41,8 +43,8 @@ class Kohonen:
 
     # x es la cantidad de atributos por cada input
 
-
-    def __init__(self, x, k, radius, initial_learning_rate, variable_learning_rate,training_data, initialized_random=True):
+    def __init__(self, x, k, radius, initial_learning_rate, variable_learning_rate, training_data,
+                 initialized_random=True):
         self.neurons = np.empty(dtype=Kohonen.Neuron, shape=(k, k))
         self.variable_learning_rate = variable_learning_rate
         self.training_data = standardize_input(training_data)
@@ -56,33 +58,28 @@ class Kohonen:
 
                 # Caso: inicializar con datos al azar
                 else:
-                    self.neurons[i][j] = Kohonen.Neuron(
-                        random.choice(self.training_data)[1])  # Ignoramos el nombre de los elementos
+                    self.neurons[i][j] = Kohonen.Neuron(copy.deepcopy(random.choice(self.training_data)[1]))
 
         self.radius = radius
         self.learning_rate = initial_learning_rate
 
-    def get_learning_rate(self,iteration=None):
+    def get_learning_rate(self, iteration=None):
         if not self.variable_learning_rate or iteration is None or iteration == 0:
             return self.learning_rate
         return self.learning_rate / iteration
 
-
     def train(self, limit, variable_radius=False):
-        size = len(self.training_data)
         for i in range(limit):
-            idx = random.randint(0, size - 1)
-            city_name, data = self.training_data[idx]
+            for city_name, data in self.training_data:
+                row, col = self.get_winner_neuron(data)
 
-            row, col = self.get_winner_neuron(data)
+                self.update_weights(row, col, data, i)
 
-            self.update_weights(row, col, data,i)
-
-            if variable_radius:
-                if self.radius > 1:
-                    self.radius -= 1
-                if self.radius < 1:
-                    self.radius = 1
+                if variable_radius:
+                    if self.radius > 1:
+                        self.radius -= 1
+                    if self.radius < 1:
+                        self.radius = 1
 
     def test(self):
 
@@ -98,19 +95,20 @@ class Kohonen:
             resp[row][col].append(city)
         return resp
 
-    def update_weights(self, row, col, current_input,iteration):
-        radius = min(self.radius,
-                     (2 ** 1 / 2) * len(self.neurons))  # actoamos radio por diagonal de la matriz, sqrt(2) * K
-        min_fil = math.floor(row - radius)
-        max_fil = math.ceil(row + radius)
-        min_col = math.floor(col - radius)
-        max_col = math.ceil(col + radius)
+    def update_weights(self, row, col, current_input, iteration):
+        # acotamos radio por diagonal de la matriz, sqrt(2) * K
+        radius = min(self.radius, len(self.neurons))
+
+        min_fil = max(math.floor(row - radius), 0)
+        max_fil = min(math.ceil(row + radius), len(self.neurons) - 1)
+        min_col = max(math.floor(col - radius), 0)
+        max_col = min(math.ceil(col + radius), len(self.neurons[0]) - 1)
 
         for current_row in range(min_fil, max_fil + 1):
             for current_col in range(min_col, max_col, + 1):
                 distance = math.sqrt((current_row - row) ** 2 + (current_col - col) ** 2)
                 # cumple radio y está dentro de la matriz
-                if distance <= radius and 0 <= current_row < len(self.neurons) and 0 <= current_col < len(self.neurons[0]):
+                if distance <= radius:
                     # se actualizan los pesos
                     neuron = self.neurons[current_row][current_col]
                     neuron.weights = neuron.weights + self.get_learning_rate(iteration) * (current_input - neuron.weights)
